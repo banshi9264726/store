1ff585b5a45c1d151ef740e3c486ed1c4f99e8ae
banshi9264726 <670039605@qq.com>
2018-06-01 19:25:47 +0800

    解决报错问题

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
 
-Vue.config.productionTip = false
+// 程序的入口
+import Vue from 'vue';
+// 根组件
+import App from './App';
+import router from './router';
+
+// 导入bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+// 导入css模块
+import '@/assets/css/index.css';
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +20,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
